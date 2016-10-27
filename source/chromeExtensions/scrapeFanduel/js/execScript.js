ef6a6c74acc1239b457123feb71de5d65f3b3dc3
Dan Doozan <dandoozan@gmail.com>
2016-10-27 02:07:19 -0400

    Improved chrome extension and scraped more contest results

--- source/chromeExtensions/scrapeFanduel/js/execScript.js
+++ source/chromeExtensions/scrapeFanduel/js/execScript.js
@@ -1,42 +1,117 @@
+var FIRST_PAGE_BUTTON_SELECTOR = 'button.page-first';
+var LAST_WINNING_PAGE_BUTTON_SELECTOR = 'button[ng-click="detail.goToLastWinningPosition()"';
 
-function getChildByClassName(parent,cl) {
-	var myclass = new RegExp('\\b'+cl+'\\b');
-	var children = parent.childNodes;
-	return getChildByClassName2(children,myclass);
-}
-function getChildByClassName2(children,myclass) {
-	for(var j=0;j<children.length;j++){
-		classe=children[j].className;
-		if (myclass.test(classe))
-			return children[j];
-		var child=getChildByClassName2(children[j].childNodes,myclass);
-		if(child)
-			return child;
-	}
+function scrapeUsers() {
+    return $('.live-leaderboard-entry');
 }
 
-function scrapeResults(){
-    results = []
+function scrapeRank(userNode) {
+    return userNode.find('.rank').text().trim()
+}
 
-    $('.live-leaderboard-entry').each(function(index, node) {
-        rank = $(node).find('.rank').text().trim()
-        username = $(node).find('.username').text().trim()
-        winnings = $(node).find('.user-winnings').text().trim()
-        score = $(node).find('.user-score').text().trim()
+function scrapeUsername(userNode) {
+    return userNode.find('.username').text().trim()
+}
 
+function scrapeWinnings(userNode) {
+    return userNode.find('.user-winnings').text().trim()
+}
+
+function scrapeScore(userNode) {
+    return userNode.find('.user-score').text().trim()
+}
+
+function scrapeCurrentPageResults() {
+    var results = [];
+
+    scrapeUsers().each(function(index, node) {
+        var $node = $(node);
         results.push({
-            rank: rank,
-            username: username,
-            winnings: winnings,
-            score: score,
+            rank: scrapeRank($node),
+            username: scrapeUsername($node),
+            winnings: scrapeWinnings($node),
+            score: scrapeScore($node),
         });
     });
 
-	return results;
+    return results;
+}
+
+function navigateToPage(buttonSelector, isFirstPage, callback) {
+    var buttons = $(buttonSelector);
+    var button = buttons.length ? buttons[0] : buttons;
+    var $button = $(button);
+
+    if ($button &&
+        $button.attr('disabled') != 'disabled') {
+
+        $button.click()
+
+        //wait for the page to load the new results
+        var intrvl = setInterval(function() {
+            var rank = scrapeRank($(scrapeUsers()[0]));
+
+            if ((isFirstPage && rank == '1st') ||
+                (!isFirstPage && rank != '1st')) {
+                    clearInterval(intrvl);
+                    callback(true);
+                    return;
+            }
+        }, 100);
+    } else {
+        callback(false);
+    }
+}
+
+function scrapeTitle() {
+    return $($('.contest-name')[0]).text().trim();
+}
+
+function scrapeEntryFee(entryDetailsNode) {
+    var entryFeeFull = entryDetailsNode.find('.contest-entry-fee dd').text().trim();
+    return entryFeeFull.substring(0, entryFeeFull.indexOf('(')).trim();
+}
+
+function scrapeEntryDetail(entryDetailsNode, selector) {
+    return entryDetailsNode.find('.' + selector + ' dd').text().trim();
+}
+
+function scrapeEntryDetails() {
+    var entryDetails = $($('.entry-details')[0]);
+    return {
+        title: scrapeTitle(),
+        type: scrapeEntryDetail(entryDetails, 'contest-type'),
+        prizes: scrapeEntryDetail(entryDetails, 'contest-prizes'),
+        entryFee: scrapeEntryFee(entryDetails),
+    }
+}
+
+function scrapeData(callback) {
+    //first, get first page
+    //then, click "Last winning position" link
+    //then, scrape the 10 results that are there
+    //thats it
+
+    navigateToPage(FIRST_PAGE_BUTTON_SELECTOR, true, function() {
+        var topResults = scrapeCurrentPageResults();
+
+        navigateToPage(LAST_WINNING_PAGE_BUTTON_SELECTOR, false, function(didNavigate) {
+            var lastWinningPositionResults = didNavigate ? scrapeCurrentPageResults() : [];
+            callback({
+                info: scrapeEntryDetails(),
+                results: {
+                    top: topResults,
+                    lastWinningPosition: lastWinningPositionResults,
+                }
+            });
+        });
+    });
 }
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	if(request.req=='scrapeResults'){
-		sendResponse(scrapeResults());
+	if(request.req=='scrapeData'){
+        scrapeData(function(data) {
+            sendResponse(data);
+        });
 	}
 });
--- source/chromeExtensions/scrapeFanduel/js/popup.js
+++ source/chromeExtensions/scrapeFanduel/js/popup.js
@@ -1,16 +1,21 @@
 
 window.onload=function(){
 	chrome.tabs.getSelected(function(tab){
-		chrome.tabs.sendRequest(tab.id, {req:'scrapeResults'}, function(results) {
-            console.log(results)
-			displayPage(results)
+		chrome.tabs.sendRequest(tab.id, {req:'scrapeData'}, function(data) {
+			displayPage(data);
 		});
 	});
 }
 
-function displayPage(results){
-    table = $('#results');
+function displayInfo(info) {
+    $('#title').text(info.title);
+    $('#type').text(info.type);
+    $('#prizes').text(info.prizes);
+    $('#entryFee').text(info.entryFee);
+}
 
+function displayResultSet(parent, results) {
+    var table = $('<table>');
     for (var i = 0; i < results.length; i++) {
         tr = $('<tr>')
         for (var item in results[i]) {
@@ -18,4 +23,23 @@ function displayPage(results){
         }
         table.append(tr)
     }
+    parent.append(table);
+}
+
+function displayPage(data){
+
+    //display contest info
+    displayInfo(data.info);
+
+    //display top results
+    resultsContainer = $('#results');
+    displayResultSet(resultsContainer, data.results.top);
+
+    //display last winning results
+    lastWinningPositionResults = data.results.lastWinningPosition;
+    if (lastWinningPositionResults.length > 0) {
+        resultsContainer.append($('<div>...</div>'))
+        displayResultSet(resultsContainer, lastWinningPositionResults);
+    }
+
 }
