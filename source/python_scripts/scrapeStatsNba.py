45beee7d5726a7329d40044763c44ef63896b8b5
Dan Doozan <dandoozan@gmail.com>
2016-11-03 14:50:19 -0400

    Scrape nba Opponent data

--- source/python_scripts/scrapeStatsNba.py
+++ source/python_scripts/scrapeStatsNba.py
@@ -4,8 +4,11 @@ import json
 import scraper
 import _util as util
 
-TYPE = 'Advanced'
-MODE = 'Totals'
+TYPE = 'Opponent' #Advanced
+MODE = 'Per100Possessions' #Totals
+BASE_URL = 'http://stats.nba.com/stats/leagueplayerondetails?'
+#BASE_URL = 'http://stats.nba.com/stats/leaguedashplayerstats?'
+
 SEASONS = {
     '2014': {
         'str': '2014-15',
@@ -18,7 +21,6 @@ SEASONS = {
         'endDate': date(2016, 4, 14),
     },
 }
-BASE_URL = 'http://stats.nba.com/stats/leaguedashplayerstats?'
 DATE_FORMAT_FILENAME = '%Y%m%d'
 ONE_DAY = timedelta(1)
 SLEEP = 10
@@ -62,6 +64,18 @@ def createUrlParams(startDate, endDate, measureType, perMode, season):
         'VsDivision': '',
         'Weight': '',
     }
+def createHeaders():
+    return {
+        'Accept': 'application/json, text/plain, */*',
+        #'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        #'Cookie': '__gads=ID=b3da99dc26c5cfd0:T=1477690842:S=ALNI_MbtFle3YGgA0oDc1DThDwtcxDAorw; crtg_trnr=; AMCVS_7FF852E2556756057F000101%40AdobeOrg=1; ug=57ed5ed1075a9d0a3c745d01dd006f88; ugs=1; AMCV_7FF852E2556756057F000101%40AdobeOrg=817868104%7CMCAID%7C2BF6AF6A051D0177-60000133C0016969%7CMCIDTS%7C17109%7CMCMID%7C14125603868183326573139779677214302359%7CMCAAMLH-1478561842%7C7%7CMCAAMB-1478757451%7CNRX38WO0n5BH8Th-nqAG_A%7CMCOPTOUT-1478159851s%7CNONE; _ga=GA1.2.475993216.1475174256; _gat=1; s_cc=true; s_fid=5F2F8D4AD9297E14-3D79A4301B91CFBF; s_sq=%5B%5BB%5D%5D; s_vi=[CS]v1|2BF6AF6A051D0177-60000133C0016969[CE]',
+        'Host':'stats.nba.com',
+        'Pragma': 'no-cache',
+        'Referer': 'http://stats.nba.com/league/player/',
+    }
 
 def getDataValues(data):
     return data['resultSets'][0]['rowSet']
@@ -71,7 +85,7 @@ def getDataValues(data):
 print 'Scraping Type=%s, Mode=%s' % (TYPE, MODE)
 
 season = raw_input('Enter season (eg. 2015): ').strip()
-parentDir = 'data/rawDataFromStatsNba/' + TYPE + '_Season/' + season
+parentDir = 'data/rawDataFromStatsNba/' + TYPE + '/' + season
 util.createDirIfNecessary(parentDir)
 
 seasonObj = SEASONS[season]
@@ -85,7 +99,7 @@ while currDate <= endDate:
 
     url = scraper.createUrl(BASE_URL, createUrlParams(startDate, currDate, TYPE, MODE, seasonObj['str']))
 
-    jsonData = scraper.downloadJson(url)
+    jsonData = scraper.downloadJson(url, createHeaders())
     #jsonData = json.load(open(PARENT_DIR + '/tbx_2015-10-27.json'))
 
     dataValues = getDataValues(jsonData)
