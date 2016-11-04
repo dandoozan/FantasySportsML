627a08b2f70a4fc1799a232d6a1758f661e5ab4e
Dan Doozan <dandoozan@gmail.com>
2016-11-04 03:24:37 -0400

    Move createTeam functions into their own file

--- source/rf.R
+++ source/rf.R
@@ -45,9 +45,10 @@ library(randomForest) #randomForest
 library(hydroGOF) #rmse
 library(ggplot2) #visualization
 library(ggthemes) #visualization
-source('source/_getData.R')
 source('../ml-common/plot.R')
 source('../ml-common/util.R')
+source('source/_getData.R')
+source('source/_createTeam.R')
 
 #Globals
 FEATURES.ID = c('Date', 'Name', 'Team')
@@ -136,200 +137,23 @@ writeSolution = function(data, yName, idName, prediction, filename, extraColName
   write.csv(solution, file=filename, row.names=F, quote=F)
 }
 
-getInitialTeam = function(data) {
-  #take the top 2 from each position (except C)
-  team = rbind(
-    data[data$Position == 'PG',][1:2,],
-    data[data$Position == 'SG',][1:2,],
-    data[data$Position == 'SF',][1:2,],
-    data[data$Position == 'PF',][1:2,],
-    data[data$Position == 'C',][1,]
-  )
-  return(team)
-}
-computePPD = function(fantasyPoints, salary) {
-  return(fantasyPoints / salary * 1000)
-}
-printPlayer = function(player) {
-  cat('    ', as.character(player$Position), ', ', sep='')
-  cat(paste(player[, c('Name', 'Salary')], collapse=', '), sep='')
-  cat(', ', round(player$FantasyPoints, 2), sep='')
-  cat(', ', round(player$PPD, 2), sep='')
-  cat('\n')
-}
-printTeam = function(team) {
-  for (i in 1:nrow(team)) {
-    printPlayer(team[i,])
-  }
-  cat('    Total Amount spent:', computeAmountSpent(team), '\n')
-  cat('    Total Fantasy Points:', computeTotalFantasyPoints(team), '\n')
-}
-computeTotalFantasyPoints = function(team) {
-  return(sum(team$FantasyPoints))
-}
-computeAmountSpent = function(team) {
-  return(sum(team$Salary))
-}
-computeAmountOverBudget = function(team) {
-  return(computeAmountSpent(team) - SALARY_CAP)
-}
-replacePlayer = function(team, oldPlayer, newPlayer) {
-  #remove old player
-  playersToKeep = setdiff(rownames(team), rownames(oldPlayer))
-  team = team[playersToKeep,]
+createTeams = function(testData, prediction, yName) {
+  cat('Creating teams...\n')
 
-  #add new player
-  team = rbind(team, newPlayer)
+  #create my team (using prediction)
+  predictionDF = testData
+  predictionDF[[yName]] = prediction
+  myTeam = createTeam(predictionDF)
+  cat('My team:\n')
+  printTeam(myTeam)
 
-  return(team)
-}
-getWorseTeam = function(data, team, amountOverBudget, verbose=F) {
-  cnt = 1
-  while (amountOverBudget > 0) {
-    if (verbose) print(paste('Iteration', cnt, ', amountOverBudget=', amountOverBudget))
+  #create best team (using test)
+  bestTeam = createTeam(testData)
+  cat('Best team:\n')
+  printTeam(bestTeam)
 
-    bestPpdg = Inf
-    bestOldPlayer = NULL
-    bestNewPlayer = NULL
-
-    positions = c('PG', 'SG', 'SF', 'PF', 'C')
-
-    #find next best player for each position
-    for (position in positions) {
-      numPlayers = ifelse(position == 'C', 1, 2)
-
-      players = data[data$Position == position,]
-
-      #remove players currently on the team
-      playersOnTeam = team[team$Position == position,]
-      players = players[setdiff(rownames(players), rownames(playersOnTeam)),]
-
-      for (i in 1:numPlayers) {
-        teamPlayer = team[team$Position == position,][i,]
-        fpDiff = teamPlayer$FantasyPoints - players$FantasyPoints
-        salaryDiff = pmin(teamPlayer$Salary - players$Salary, amountOverBudget)
-        salaryDiff[salaryDiff <= 0] = NA
-        ppdg = computePPD(fpDiff, salaryDiff)
-        minPpdg = min(ppdg, na.rm=T)
-        if (minPpdg < bestPpdg) {
-          bestPpdg = minPpdg
-          bestOldPlayer = teamPlayer
-          bestNewPlayer = players[which.min(ppdg), ]
-        }
-      }
-    }
-
-    #i now have the next best player, replace him
-    if (verbose) cat('Replacing', oldPlayer$Name, '<-', newPlayer$Name, '\n')
-    team = replacePlayer(team, bestOldPlayer, bestNewPlayer)
-
-    amountOverBudget = computeAmountOverBudget(team)
-    cnt = cnt + 1
-  }
-  return(team)
-}
-getBetterTeam = function(data, team, amountUnderBudget, verbose=F) {
-  #while amountOverBudget < 0, look for a better match
-  #compute the next highest ppdg for each position
-  #select the matchup with the highest ppdg gained
-  cnt = 1
-  someoneWasReplaced = TRUE
-  while (amountUnderBudget > 0 && someoneWasReplaced) {
-    someoneWasReplaced = FALSE
-    if (verbose) cat(paste('Iteration', cnt, ', amountUnderBudget=', amountUnderBudget), '\n')
-
-    bestPpdg = -Inf
-    bestOldPlayer = NULL
-    bestNewPlayer = NULL
-
-    positions = c('PG', 'SG', 'SF', 'PF', 'C')
-
-    #find next best player for each position
-    for (position in positions) {
-      numPlayers = ifelse(position == 'C', 1, 2)
-
-      players = data[data$Position == position,]
-
-      #remove players currently on the team
-      playersOnTeam = team[team$Position == position,]
-      players = players[setdiff(rownames(players), rownames(playersOnTeam)),]
-
-      for (i in 1:numPlayers) {
-        teamPlayer = team[team$Position == position,][i,]
-        fpDiff = players$FantasyPoints - teamPlayer$FantasyPoints
-        salaryDiff = players$Salary - teamPlayer$Salary
-        salaryDiff[salaryDiff > amountUnderBudget] = NA
-        salaryDiff[salaryDiff < 0] = NA
-        if (sum(is.na(salaryDiff)) != length(salaryDiff)) {
-          ppdg = computePPD(fpDiff, salaryDiff)
-          maxPpdg = max(ppdg, na.rm=T)
-          if (maxPpdg > bestPpdg) {
-            bestPpdg = maxPpdg
-            bestOldPlayer = teamPlayer
-            bestNewPlayer = players[which.max(ppdg), ]
-          }
-        }
-      }
-    }
-
-    if (!is.null(bestOldPlayer)) {
-      someoneWasReplaced = TRUE
-      #i now have the next best player, replace him
-      if (verbose) cat('Replacing', bestOldPlayer$Name, '<-', bestNewPlayer$Name, '\n')
-      team = replacePlayer(team, bestOldPlayer, bestNewPlayer)
-    }
-
-    if (verbose) {
-      printTeam(team)
-      cat('\n')
-    }
-
-    amountUnderBudget = -computeAmountOverBudget(team)
-    cnt = cnt + 1
-  }
-  return(team)
-}
-createTeam = function(data, verbose=F) {
-  #add PPD coumn
-  data$PPD = computePPD(data$FantasyPoints, data$Salary)
-
-  #sort by ppd
-  data = data[order(data$PPD, decreasing=TRUE),]
-
-  #first, fill team with all the highest ppd players
-  team = getInitialTeam(data)
-
-  if (verbose) {
-    cat('Initial team:\n')
-    printTeam(team)
-  }
-
-  amountOverBudget = computeAmountOverBudget(team)
-  if (amountOverBudget > 0) {
-    team = getWorseTeam(data, team, amountOverBudget, verbose)
-  } else if (amountOverBudget < 0) {
-    team = getBetterTeam(data, team, -amountOverBudget, verbose)
-  } else {
-    cat('Wow, I got a perfect team on the first try!\n')
-  }
-
-  if (verbose) {
-    cat('Final team:\n')
-    printTeam(team)
-  }
-
-  return(team)
-}
-printTeamResults = function(team, bestTeam, yName) {
-  myTeamPredictedPoints = sum(myTeam[[yName]])
-  myTeamActualPoints = sum(test[rownames(myTeam), yName])
-  bestTeamPoints = sum(bestTeam[[yName]])
-
-  cat('How did my team do?\n')
-  cat('    I was expecting to get', myTeamPredictedPoints, 'points\n')
-  cat('    I actually got:', myTeamActualPoints, 'points\n')
-  cat('    Best team got:', bestTeamPoints, 'points\n')
-  cat('    My score ratio is', (myTeamActualPoints/bestTeamPoints), '\n')
+  #print myTeam / bestTeam ratio
+  printTeamResults(myTeam, bestTeam, yName)
 }
 
 #============= Main ================
@@ -362,24 +186,8 @@ tbx_commentsToCollapse = function() {
 # prediction = createPrediction(model, test, featuresToUse)
 # testError = computeError(test[, Y_NAME], prediction)
 # cat('    Tonight\'s error: ', testError, '\n', sep='')
-
-#comment this out for now
-# cat('Creating teams...\n')
-# SALARY_CAP = 60000
-# #create my team (using prediction)
-# predictionDF = test
-# predictionDF[[Y_NAME]] = prediction
-# myTeam = createTeam(predictionDF)
-# cat('My team:\n')
-# printTeam(myTeam)
 #
-# #create best team (using test)
-# bestTeam = createTeam(test)
-# cat('Best team:\n')
-# printTeam(bestTeam)
-#
-# #print myTeam / bestTeam ratio
-# printTeamResults(team, bestTeam, Y_NAME)
+# createTeams(test, prediction, Y_NAME)
 
 #comment this out because i think i dont need it
 #if (PROD_RUN) {
