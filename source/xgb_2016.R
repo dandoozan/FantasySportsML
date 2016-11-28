1970779474627829f4354d19dbcf56ef341ea34d
Dan Doozan <dandoozan@gmail.com>
2016-11-28 18:51:13 -0500

    Retune xgb params slightly, meanrmse=8.085093

--- source/xgb_2016.R
+++ source/xgb_2016.R
@@ -64,10 +64,10 @@ getHyperParams = function() {
     gamma = 0,#0 #Larger value=less overfitting
 
     #range=[1,∞], default=6, toTry=3,5,7,9,12,15,17,25
-    max_depth = 2,#10 #Lower value=less overfitting
+    max_depth = 3,#10 #Lower value=less overfitting
 
     #range=(0,1], default=1, toTry=0.6,0.7,0.8,0.9,1.0
-    subsample = 1,#1 #ratio of sample of data to use for each instance (eg. 0.5=50% of data). Lower value=less overfitting
+    subsample = 0.9,#1 #ratio of sample of data to use for each instance (eg. 0.5=50% of data). Lower value=less overfitting
 
     #range=[0,∞], default=1, toTry=1,3,5,7
     min_child_weight = 5,#5 #Larger value=less overfitting
