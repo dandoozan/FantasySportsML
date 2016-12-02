8a73931e12bf1c1da1a70d3cf87dc5bb396ca334
Dan Doozan <dandoozan@gmail.com>
2016-12-02 18:03:55 -0500

    [glm] Use log(y) as y, meanrmse=0.5606038

--- source/glm_2016.R
+++ source/glm_2016.R
@@ -1,13 +1,20 @@
+
+#D-Use RG_points, NF_FP: Trn/CV/Train=8.704967/9.173133/8.800143, RG=9.21892/9.173133, NF=9.654601/9.173133, -$4, 7/9, 8.833883/8.953031/10.05593, 0.9343918
+#D-Use log(y): 0.5597699/0.5754786/0.5629384, 0.578914/0.5754786, 0.6198487/0.5754786, -$8, 6/10, 0.5606038/0.5651253/0.4276129, 0.9191064
+
 #================= Functions ===================
 createModel = function(d, yName, xNames, amountToAddToY) {
   set.seed(754)
-  return(cv.glmnet(x=as.matrix(d[, xNames]), y=d[[yName]]))
+  #return(cv.glmnet(x=as.matrix(d[, xNames]), y=d[[yName]]))
+  return(cv.glmnet(x=as.matrix(d[, xNames]), y=log(d[[yName]] + amountToAddToY)))
 }
 createPrediction = function(model, newData, xNames, amountToAddToY) {
-  return(predict(model, as.matrix(newData[, xNames]), s='lambda.min')[,1])
+  #return(predict(model, as.matrix(newData[, xNames]), s='lambda.min')[,1])
+  return(exp(predict(model, as.matrix(newData[, xNames]), s='lambda.min')[,1]) - amountToAddToY)
 }
 computeError = function(y, yhat, amountToAddToY) {
-  return(rmse(y, yhat))
+  #return(rmse(y, yhat))
+  return(rmse(log(y + amountToAddToY), log(yhat + amountToAddToY)))
 }
 printModelResults = function(model) {
   return()
