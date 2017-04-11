ec0bbde7859779f23917babadd6c1dcac831f2f1
Dan Doozan <dandoozan@gmail.com>
2017-04-11 18:29:02 -0400

    Fix stats nba request, and add am and pm raw data

--- source/python_scripts/scrapeStatsNba.py
+++ source/python_scripts/scrapeStatsNba.py
@@ -230,11 +230,13 @@ def createHeaders(hdrs):
         #'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'en-US,en;q=0.8',
         'Cache-Control': 'no-cache',
+        'Cookie': 'ug=58d86b960c26f00a3c8f7873980084c7; ugs=1; _ga=GA1.2.624056311.1491942990; s_cc=true; s_sq=%5B%5BB%5D%5D; s_vi=[CS]v1|2C769F260519410C-600006056003B198[CE]; s_fid=657B70FBDE2E63B6-0634C28D0F3D7667',
         'Connection': 'keep-alive',
-        #'Cookie': '__gads=ID=b3da99dc26c5cfd0:T=1477690842:S=ALNI_MbtFle3YGgA0oDc1DThDwtcxDAorw; crtg_trnr=; AMCVS_7FF852E2556756057F000101%40AdobeOrg=1; ug=57ed5ed1075a9d0a3c745d01dd006f88; ugs=1; AMCV_7FF852E2556756057F000101%40AdobeOrg=817868104%7CMCAID%7C2BF6AF6A051D0177-60000133C0016969%7CMCIDTS%7C17109%7CMCMID%7C14125603868183326573139779677214302359%7CMCAAMLH-1478561842%7C7%7CMCAAMB-1478757451%7CNRX38WO0n5BH8Th-nqAG_A%7CMCOPTOUT-1478159851s%7CNONE; _ga=GA1.2.475993216.1475174256; _gat=1; s_cc=true; s_fid=5F2F8D4AD9297E14-3D79A4301B91CFBF; s_sq=%5B%5BB%5D%5D; s_vi=[CS]v1|2BF6AF6A051D0177-60000133C0016969[CE]',
         'Host':'stats.nba.com',
         'Pragma': 'no-cache',
-        'Referer': 'http://stats.nba.com/league/player/',
+        'Referer': 'http://stats.nba.com/players/traditional/',
+        'x-nba-stats-origin': 'stats',
+        'x-nba-stats-token': 'true',
     }
     headers.update(hdrs)
     return headers
